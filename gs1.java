374417ee010d3ca2857958778de0e55555971c92
KarthikRajanG <karthikrajan23963@gmail.com>
2017-06-15 09:26:13 +0530

    Add files via upload

---
+++ gs1.java
@@ -0,0 +1,7 @@
+public class reverse{
+	public static void main(String[] args){
+		   String s1="karthik";
+		   String reverse=new StringBuffer(s1).reverse().toString();
+		   System.out.println("the reversed string is: "+reverse);
+		}
+		}
