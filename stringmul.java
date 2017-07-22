7c99de1987c3dcbff78af46239fbafd976f37c5b
KarthikRajanG <karthikrajan23963@gmail.com>
2017-07-22 11:24:20 +0530

    Add files via upload

---
+++ stringmul.java
@@ -0,0 +1,11 @@
+public class StringMul {
+public static void main(String[] args) {
+	String s1="25";
+	String s2="25";
+    int i1=Integer.parseInt(s1);
+    int i2=Integer.parseInt(s2);
+    int temp=i1*i2;
+    String result=String.valueOf(temp);
+    System.out.println(result);
+	}
+}
